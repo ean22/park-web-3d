8c3b627ace48309f871fb52ea0b6d75fcdac540b
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-18 21:42:27 +0000

    fix:shadow

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer( {antialias:true} );
 renderer.setSize( size.width, size.height );
 renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
+renderer.toneMapping = THREE.AgXToneMapping;
+renderer.toneMappingExposure = 1.2;
 
 document.body.appendChild( renderer.domElement );
 
@@ -44,22 +46,45 @@ controls.update();
 
 
 // Modelo 3D ________________________________________________________________________
+// const intersectObjects = [];
+// const intersectObjectsNames = [
+//     "Scene",
+//     "Plane",
+//     "Plane_1",
+//     "Plane_2",
+//     "Plane_3",
+//     "Plane_4",
+//     "Plane_5",
+//     "Plane_6",
+//     "Plane_7",
+//     "Plane_8",
+//     "Plane008",
+//     "Plane008_1",
+//     "painel",
+//     "Cube012",
+//     "Cube012_1",
+//     "Cube012_2",
+//     "Cube012_3"
+// ];
+
 
 const loader = new GLTFLoader();
 loader.load( 
     './public/park.glb', 
     function ( glb ) {
         glb.scene.traverse( child => {
-            console.log( child );
-
+            // if( intersectObjectsNames.includes( child.name )){
+            //     intersectObjects.push( child );
+            // }
+            
             if( child.isMesh ){
                 child.castShadow = true; 
                 child.receiveShadow = true;
             } 
-        } )
+            // console.log(child);
+        } );
         
         scene.add( glb.scene );
-
     }, 
     undefined, 
     function ( error ) {
@@ -73,7 +98,8 @@ loader.load(
 // Luz ________________________________________________________________________
 
 const sun = new THREE.DirectionalLight( 0xFFFFFF );
-sun.position.set( 20, 12, 0); 
+sun.position.set( 10, 12, 0); 
+sun.target.position.set( 0, 0, 0 )
 
 scene.add( sun );
 
@@ -87,37 +113,71 @@ scene.add( light );
 // Sombras ________________________________________________________________________
 
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
 sun.castShadow = true;
 sun.shadow.camera.left = -8;
 sun.shadow.camera.right = 8;
+sun.shadow.normalBias = .02;
+sun.shadow.mapSize.height = 1024;
+sun.shadow.mapSize.width = 1024;
 
 const shadowHelper = new THREE.CameraHelper( sun.shadow.camera );
 scene.add( shadowHelper );
 
+// Raycaster ________________________________________________________________________
 
-// Loop ________________________________________________________________________
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
-function handleResize(){
+
+
+// Handlers de Eventos ________________________________________________________________________
+
+function onResize(){
     size.width = window.innerWidth;
     size.height = window.innerHeight;
-
+    
     const aspect = size.width / size.height;
     -aspect * frustum - frustumLeftRightCorrection,
     aspect * frustum - frustumLeftRightCorrection,
     camera.top = frustum;
     camera.bottom = -frustum;
-
+    
     camera.updateProjectionMatrix();
-
-    renderer.setSize(size.width, size.height)
-    renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) )
+    
+    renderer.setSize(size.width, size.height);
+    renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
 }   
 
-window.addEventListener( "resize", handleResize )
+window.addEventListener( "resize", onResize );
+
+
+function onPointerMove( event ) {
+    pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    pointer.y = ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+window.addEventListener( "pointermove", onPointerMove );
+
+
+// Loop ________________________________________________________________________
 
 function animate() {
+    raycaster.setFromCamera( pointer, camera );
+
+    const intersects = raycaster.intersectObjects( scene.children );
+
+    if( intersects.length > 0 ){
+        document.body.style.cursor = "pointer";
+    }
+
+    for( let i = 0; i < intersects.length; i++ ){
+        console.log( intersects[ 0 ].object.parent.name );
+    }
+
     renderer.render( scene, camera );
-    // console.log( camera.position );
 }
+    
 
 renderer.setAnimationLoop( animate );
